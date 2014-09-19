ed7e8e99d2134e56ee0cba14054bb5f3ef09893d
James Cartwright <james@perspectivedesign.co.uk>
2014-09-19 22:53:17 +0100

    Initial Push of theme

---
+++ functions.php
@@ -0,0 +1,40 @@
+<?php
+//* Start the engine
+include_once( get_template_directory() . '/lib/init.php' );
+
+//* Child theme (do not remove)
+define( 'CHILD_THEME_NAME', 'Genesis Sample Theme' );
+define( 'CHILD_THEME_URL', 'http://www.studiopress.com/' );
+define( 'CHILD_THEME_VERSION', '2.1.2' );
+
+//* Enqueue Google Fonts
+add_action( 'wp_enqueue_scripts', 'genesis_sample_google_fonts' );
+function genesis_sample_google_fonts() {
+
+	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Lato:300,400,700', array(), CHILD_THEME_VERSION );
+
+}
+
+//* Add HTML5 markup structure
+add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list' ) );
+
+//* Add viewport meta tag for mobile browsers
+add_theme_support( 'genesis-responsive-viewport' );
+
+//* Add support for custom background
+add_theme_support( 'custom-background' );
+
+//* Add support for 3-column footer widgets
+add_theme_support( 'genesis-footer-widgets', 3 );
+
+//* Dequeue default genesis stylesheet
+remove_action( 'genesis_meta', 'genesis_load_stylesheet' );
+
+/**
+ * Proper way to enqueue scripts and styles
+ */
+function purecss_styles() {
+	wp_enqueue_style( 'pure-css', get_stylesheet_directory_uri() . '/css/screen.css', array(), CHILD_THEME_VERSION );
+}
+
+add_action( 'wp_enqueue_scripts', 'purecss_styles' );
